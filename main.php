28e9812932bbcaf64c3cc90f8a8bba606ffca315
cadumiranda <43431628+cadu-miranda@users.noreply.github.com>
2020-12-19 05:12:45 -0300

    Update main.php

--- main.php
+++ main.php
@@ -9,7 +9,7 @@ $data ="
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta http-equiv='refresh' content='5'>
+    <meta http-equiv='refresh' content='2'>
     <title>Projeto de TCC</title>
     <style> 
         table,
